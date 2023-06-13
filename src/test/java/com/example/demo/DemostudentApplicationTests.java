8ebc203020d3616efb38ef76eeaab2fdf0195ff5
suraj kumar singh <singhsuraj9122@gmail.com>
2023-06-13 12:58:53 +0100

    application properties configured

---
+++ src/test/java/com/example/demo/DemostudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemostudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
