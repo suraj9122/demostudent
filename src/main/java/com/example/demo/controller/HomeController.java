ceaa96fe3b49c128682fe8d474a56ddf63fbaa9d
suraj kumar singh <singhsuraj9122@gmail.com>
2023-06-24 03:52:03 +0100

    exceptions handled. error page created. had an error on record not found.resolved..update page left

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,97 +1,114 @@
 package com.example.demo.controller;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.UserDetails;
 import com.example.demo.service.UserService;
 
-@Controller
+@RestController
 public class HomeController {
 	
 	@Autowired
 	private UserService userService;
 	
 	@RequestMapping("/home")
-	public String home()
+	public ModelAndView home()
 	{
 		
-		
-		return"home.html";
+		ModelAndView mv1=new  ModelAndView("home.html");
+		return mv1;
 	}
 	
 	@RequestMapping("/register")
-	public String register()
+	public ModelAndView register()
 	{
+		ModelAndView mv2=new ModelAndView();
+		mv2.addObject("register.html");
 		
-		
-		return"register.html";
+		return mv2;
 	}
 	@RequestMapping("/userdata")
-	public String userdata(@RequestParam("id")  int id,@RequestParam("password")  String pw,Model m)
+	public ModelAndView userdata(@RequestParam("id")  int id,@RequestParam("password")  String pw,Model m)
 	
 	{
 		
-		String username = "John Doe";
-       // m.addAttribute("username", username);
-	// ModelAndView model=new ModelAndView("userdata.html");
+		String msg ="";
+      
+	ModelAndView mv=new  ModelAndView();
 		UserDetails use=userService.fetchuser(id);
-		m.addAttribute("use",use);
-		 //model.addObject("use",use);
-		
-		//session.setAttribute("msg","something wrong on server");
-		// System.out.print(model);
-		/*if(id==use.getId())
-		{
-			if(pw.equals(use.getPassword()))
-			{
+		mv.addObject("use", use);
+		int sid=use.getId();
+		String spw=use.getPassword();
+			if (id ==sid && spw.equals(pw)) {
+				mv.addObject(use);
+				mv.addObject("userdata.html");
+	            return mv; // Return the name of your home HTML template
+	        } 
 			
-				
-			}
 			else
-				model.addAttribute(pw, use)
-		}
-		else
-			session.setAttribute("msg", "id wrong");
+			{
+				mv.setViewName("home.html");
+			}
+			
+			
+	            return mv; // Return the name of your login HTML template
+	        
 		
-	}*/
-		 return "userdata";
 	}
 	@RequestMapping("/createuser")
-	public String createuser(@ModelAttribute ("user")UserDetails user,HttpSession session)
+	public ModelAndView createuser(@ModelAttribute ("user")UserDetails user,HttpSession session)
 	{
 		//@RequestParam("name")  String nm,@RequestParam("address")  String ad,@RequestParam("contact")  String ct,@RequestParam("email")  String em,@RequestParam("course")  String c
 		//System.out.println(nm);
 	//	System.out.println(ad);
 	//	System.out.println(ct);
    //System.out.println(em);
+		ModelAndView mv=new  ModelAndView("redirect:/register");
 		
 		boolean f=userService.checkEmail(user.getEmail());
 		
 	
 		if (f)
 		{
-			session.setAttribute("msg", "email id already exit");
+			mv.addObject("msg", "email id already exit");
 		}
 		else
 		{
 		UserDetails use=userService.createUser(user);
 		if(use!=null) {
-		session.setAttribute("msg", "registered successfully");}
+			mv.addObject("msg", "registered successfully");}
 		else {
-			session.setAttribute("msg","something wrong on server");
+			mv.addObject("msg","something wrong on server");
 		}
 		}
 		
-		return"redirect:/register";
+		
+		return mv;
+	}
+
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ModelAndView exp() {
+		ModelAndView mv=new ModelAndView();
+		
+		mv.setViewName("error.html");
+		mv.addObject("msg","id or password wrong");
+		
+		return mv;
+				
 	}
 
 }
