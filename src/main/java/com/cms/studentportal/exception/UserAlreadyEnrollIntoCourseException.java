d9eeb1ebb5369f69afe8ef71a3bed27e4a53e82e
suraj kumar singh <singhsuraj9122@gmail.com>
2023-08-03 15:47:24 +0100

    amend

--- src/main/java/com/cms/studentportal/exception/UserAlreadyEnrollIntoCourseException.java
+++
@@ -1,9 +0,0 @@
-package com.cms.studentportal.exception;
-
-public class UserAlreadyEnrollIntoCourseException extends Exception{
-
-	public UserAlreadyEnrollIntoCourseException(String courseTitle){
-		super("User has already enrol into this course "+courseTitle);
-	}
-	
-}
--- src/main/java/com/cms/studentportal/thirdPartyApi/constant/PaymentType.java
+++
@@ -1,8 +0,0 @@
-package com.cms.studentportal.thirdPartyApi.constant;
-
-/**
- * Invoice type for finance API
- */
-public enum PaymentType {
-	TUITION_FEES;
-}
